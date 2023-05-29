537652b28fa98d13d71629266f89042132ff1f25
EndKn1ght <nadiyarahmi123@gmail.com>
2023-05-29 14:21:19 +0800

    added data store

---
+++ app/src/main/java/com/bangkit/bangkitcapstone/model/data/local/store/DataStoreManager.kt
@@ -0,0 +1,22 @@
+package com.bangkit.bangkitcapstone.model.data.local.store
+
+import androidx.datastore.core.DataStore
+import java.util.prefs.Preferences
+
+class DataStoreManager private constructor(private val dataStore: DataStore<Preferences>) {
+
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DataStoreManager? = null
+
+        fun getInstance(dataStore: DataStore<Preferences>): DataStoreManager {
+            return INSTANCE ?: synchronized(this) {
+                val instance = DataStoreManager(dataStore)
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+}
