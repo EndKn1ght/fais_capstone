254158f8162eec3696a9b96ad2013af644bae53a
istaqom <istaqom@gmail.com>
2023-06-08 15:01:00 +0800

    Using .dotenv

--- database.js
+++ database.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const db_host = process.env.DATABASE_HOST;
+const db_user = process.env.DATABASE_USER;
+const db_pass = process.env.DATABASE_PASS;
+const db_name = process.env.DATABASE_NAME;
 
 // Replace the database credentials with your own
-const sequelize = new Sequelize('capstone', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize(db_name, db_user, db_pass, {
+  host: db_host,
   dialect: 'mysql',
 });
 
